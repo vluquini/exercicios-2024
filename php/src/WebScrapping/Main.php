f40f40b1608367e505b35dd79f34609ad53eff04
Vinicius Nunes <theviniciusluquini@gmail.com>
2024-04-11 23:05:18 -0300

    feat: finalização do projeto para implementação de webscrapping

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,6 +2,7 @@
 
 namespace Chuva\Php\WebScrapping;
 
+use Chuva\Php\WebScrapping\CreateXlsx;
 /**
  * Runner for the Webscrapping exercice.
  */
@@ -12,12 +13,10 @@ class Main {
    */
   public static function run(): void {
     $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html', LIBXML_NOERROR);
 
     $data = (new Scrapper())->scrap($dom);
-
-    // Write your logic to save the output file bellow.
-    print_r($data);
+    $xlsx = (new CreateXlsx())->create($data);
   }
 
 }
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -10,21 +10,72 @@ use Chuva\Php\WebScrapping\Entity\Person;
  */
 class Scrapper {
 
-  /**
-   * Loads paper information from the HTML and returns the array with the data.
-   */
-  public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
-  }
-
+    /**
+     * @throws WriterNotOpenedException
+     * @throws IOException
+     * @throws InvalidArgumentException
+     */
+    public function scrap(\DOMDocument $dom): array {
+      $papers = [];
+  
+      // Seleciona todas as divs com a classe específica.
+      $paperDivs = $dom->getElementsByTagName('div');
+      foreach ($paperDivs as $paperDiv) {
+        // Procurando a classe específica para resgatar os dados.
+        if ($paperDiv->getAttribute('class') === 'col-sm-12 col-md-8 col-lg-8 col-md-pull-4 col-lg-pull-4') {
+          // Extrai os trabalhos dentro div <a>.
+          $paperCards = $paperDiv->getElementsByTagName('a');
+          foreach ($paperCards as $paperCard) {
+            // Extrai o id.
+            $id = '';
+            $elements = $paperCard->getElementsByTagName('div');
+            foreach ($elements as $element) {
+              if ($element->getAttribute('class') === 'volume-info') {
+                $id = $element->nodeValue;
+                break;
+              }
+            }
+  
+            // Extrai o título do trabalho.
+            $title = '';
+            $titleElement = $paperCard->getElementsByTagName('h4')->item(0);
+            if ($titleElement !== NULL) {
+              $title = $titleElement->nodeValue;
+            }
+  
+            // Extrai o tipo do trabalho.
+            $type = '';
+            $typeElements = $paperCard->getElementsByTagName('div');
+            foreach ($typeElements as $typeElement) {
+              if ($typeElement->getAttribute('class') === 'tags mr-sm') {
+                $type = $typeElement->nodeValue;
+                break;
+              }
+            }
+  
+            // Extrai os autores do trabalho.
+            $authors = [];
+            $authorsElement = $paperCard->getElementsByTagName('div')->item(0);
+            if ($authorsElement !== null) { 
+              $authorsSpans = $authorsElement->getElementsByTagName('span');
+              foreach ($authorsSpans as $authorSpan) {
+                $name = $authorSpan->nodeValue;
+                $institution = $authorSpan->getAttribute('title');
+                $authors[] = new Person($name, $institution);
+              }
+            }
+      
+            // Cria um objeto Paper e adiciona ao array de papers.
+            $papers[] = new Paper(
+              $id,
+              $title,
+              $type,
+              $authors
+            );
+          }
+        }
+      }
+      return $papers;
+    }
+  
 }
